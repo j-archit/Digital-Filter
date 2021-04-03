87b461605d876c598f9237f91c7aebab10ed2224
j-archit <archit.jain38@gmail.com>
2021-04-04 01:18:27 +0530

    Added Coeffs Input from File into Register Array

---
+++ iir_order2.v
@@ -0,0 +1,31 @@
+/*
+    Digital Filter Implementation
+    Digital Signal Processing - MTE Project
+
+    Parameters:
+        1. bitwidth     : Width of the Encoded Sequence's Sample (Default, 32)
+    Inputs:
+        1. clk          : Clock Input
+        2. x            : Input Stream (Encoded Signal Integer Stream)
+    
+    Outputs:
+        3. y            : Output Stream (Encoded Signal Integer Stream)
+    
+*/
+
+module iir_order2
+#(parameter bitwidth = 32, parameter coeffs_file = "1.txt")
+(
+    input clk,
+    input [bitwidth-1:0] x,
+    output [bitwidth-1:0] y
+);
+
+    reg [bitwidth-1:0] coeffs [0:4];
+    initial begin
+        $readmemh(coeffs_file, coeffs);
+    end
+
+    // Filter Code Here
+
+endmodule
