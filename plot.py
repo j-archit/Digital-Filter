b1d6312af6d785933d653840af44a834bd025d1f
j-archit <archit.jain38@gmail.com>
2021-04-05 03:22:07 +0530

    plot changes

--- plot.py
+++ plot.py
@@ -2,6 +2,9 @@ import csv
 import codecs
 import matplotlib.pyplot as plt
 
+opcolor = '#ffffff'
+opcolor2 = '#44c4d4'
+
 unfil = []
 fil = []
 
@@ -16,13 +19,37 @@ with codecs.open("output.txt", "r", "utf-8") as f:
         except:
             continue
 
+# Unfiltered Input Sequence
 plt.figure()
-plt.subplot(211)
-plt.plot(unfil)
-plt.ylabel("unfiltered")
+ax = plt.axes()
+ax.spines['top'].set_visible(False)
+ax.spines['right'].set_visible(False)
+ax.spines['bottom'].set_color(opcolor2)
+ax.spines['left'].set_color(opcolor2)
+ax.yaxis.label.set_color(opcolor2)
+ax.xaxis.label.set_color(opcolor2)
+ax.tick_params(length = 0, labelcolor = opcolor2, labelrotation = 45, label1On = False, label2On = False)
+ax.set_aspect(0.05)
 
-plt.subplot(212)
-plt.plot(fil)
-plt.ylabel("filtered")
+plt.plot(unfil, color = opcolor)
+plt.ylabel("Input")
+plt.xlabel("Samples")
+plt.savefig("unf.png", transparent = True, dpi = 1200)
 
-plt.savefig("plot.png")
+
+# Filtered Output Sequence
+plt.figure()
+ax1 = plt.axes()
+ax1.spines['top'].set_visible(False)
+ax1.spines['right'].set_visible(False)
+ax1.spines['bottom'].set_color(opcolor2)
+ax1.spines['left'].set_color(opcolor2)
+ax1.yaxis.label.set_color(opcolor2)
+ax1.xaxis.label.set_color(opcolor2)
+ax1.tick_params(length = 0, labelcolor = opcolor2, labelrotation = 45, label1On = False, label2On = False)
+ax1.set_aspect(0.1)
+
+plt.plot(fil, color = opcolor)
+plt.ylabel("Filtered Output")
+plt.xlabel("Samples")
+plt.savefig("fil.png", transparent = True, dpi = 1200)
