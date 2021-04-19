cc02e2abdd9176e56421cca7b686bf9228f08cce
Divyanshujain2000 <2000divyanshujain@gmail.com>
2021-04-19 16:01:49 +0530

    edit1

--- iir_order2.v
+++ iir_order2.v
@@ -1,17 +1,12 @@
+`timescale 1ns / 1ps
 /*
     Digital Filter Implementation
     Digital Signal Processing - MTE Project
 
     Parameters:
-        1. index        : Section Index in Nth order Filter.
-            (Default, 0)
-            
-            If module is used as Stand Alone, default value is to be used.
-            This Index is used to get Coefficients into Memory using the file with the name "<index>"
-            Index should be in the format "xx" (two digits only)
-
-        2. bitwidth     : Width of the Encoded Sequence's Sample (Optional)
-            (Default, 32)
+        1. coeffs_file  : File Containing 5 filter coefficients in Binary Integer Format
+            (0-4 Index corresponds to b0->b2, a1, a2 respectively)
+        2. bitwidth     : Width of the Encoded Sequence's Sample (Default, 32, Optional)
 
     Inputs:
         1. clk          : Clock Input
@@ -36,13 +31,13 @@
                 |           |           |
                 |----(-a2)------(b2)----| 
 
-    Internal Wires/Registers:
-
-        (0-4 Index corresponds to b0->b2, a1, a2 respectively)
 */
 
-module iir_2
-#(parameter index = 0,  parameter bitwidth = 32, parameter fac = 20, parameter gain = 6)
+
+
+
+module iir_order2
+#(parameter coeffs_file = "mem_coff.mem", parameter bitwidth = 32)
 (
     input clk,
     input rst,
@@ -52,25 +47,27 @@ module iir_2
 
     // Filter Structure Wires and Registers
     reg signed [bitwidth-1:0] C [0:4];
+	 /*
+		C[0] = 	 b0
+		C[1] = 	 b1
+		C[2] = 	 b2
+		C[3] = 	-a1
+		C[4] =	-a2
+	*/
     reg signed [2*bitwidth-1:0] z [1:2];
     wire signed [2*bitwidth-1:0] w [1:2];
 
-    // For Coeff File derived from Index value
-    reg [2*8:1] cfile = "xx";
-    integer t0, t1;
     initial begin
-        t0 = index % 10;
-        t1 = index / 10;
-        cfile[8:1] = "0" + t0;
-        cfile[16:9] = "0" + t1;
-        $readmemb(cfile, C);
-        //$display("Index = %d,\nb[0] = %d, \nb[1] = %d, \nb[2] = %d, \na[1] = %d, \na[2] = %d\n", index, C[0], C[1], C[2], C[3], C[4]);
+        $readmemb(coeffs_file, C);
+        z[1] = {2*bitwidth{1'b0}};
+        z[2] = {2*bitwidth{1'b0}};
+        //$display("0:b[0] = %d, \n1:b[1] = %d, \n2:b[2] = %d, \n3:a[1] = %d, \n4:a[2] = %d\n", C[0], C[1], C[2], C[3], C[4]);
     end
 
     // Filter Code
-    assign w[1] = ((x <<< fac) - C[3]*z[1] - C[4]*z[2]) >>> fac;
-    assign w[2] = (C[0]*w[1] + C[1]*z[1] + C[2]*z[2]) >>> fac;
-    assign y = w[2] <<< gain;
+    assign w[1] = ((x <<< 20) - C[3]*z[1] - C[4]*z[2]) >>> 20;
+    assign w[2] = (C[0]*w[1] + C[1]*z[1] + C[2]*z[2]) >>> 20;
+    assign y = w[2] >>> 3;
 
     always @(posedge(clk)) begin
         if(rst == 1) begin
@@ -78,8 +75,8 @@ module iir_2
             z[2] = {2*bitwidth{1'b0}};
         end
         else begin
-            z[1] <= w[1];
-            z[2] <= z[1];
+            z[1] = w[1];
+            z[2] = z[1];
         end;
     end
 endmodule
--- plot.py
+++ plot.py
@@ -2,12 +2,6 @@ import csv
 import codecs
 import matplotlib.pyplot as plt
 
-opcolor = '#000000'
-opcolor2 = '#000000'
-aspin = 0.3
-aspout = 0.1
-DPI = 300
-
 unfil = []
 fil = []
 
@@ -22,37 +16,13 @@ with codecs.open("output.txt", "r", "utf-8") as f:
         except:
             continue
 
-# Unfiltered Input Sequence
 plt.figure()
-ax = plt.axes()
-ax.spines['top'].set_visible(False)
-ax.spines['right'].set_visible(False)
-ax.spines['bottom'].set_color(opcolor2)
-ax.spines['left'].set_color(opcolor2)
-ax.yaxis.label.set_color(opcolor2)
-ax.xaxis.label.set_color(opcolor2)
-ax.tick_params(length = 0, label1On = False, label2On = False)
-#ax.set_aspect(aspin)
+plt.subplot(211)
+plt.plot(unfil)
+plt.ylabel("unfiltered")
 
-plt.plot(unfil, color = opcolor)
-plt.ylabel("Input")
-plt.xlabel("Samples")
-plt.savefig("unf.png", transparent = True, dpi = DPI)
+plt.subplot(212)
+plt.plot(fil)
+plt.ylabel("filtered")
 
-
-# Filtered Output Sequence
-plt.figure()
-ax1 = plt.axes()
-ax1.spines['top'].set_visible(False)
-ax1.spines['right'].set_visible(False)
-ax1.spines['bottom'].set_color(opcolor2)
-ax1.spines['left'].set_color(opcolor2)
-ax1.yaxis.label.set_color(opcolor2)
-ax1.xaxis.label.set_color(opcolor2)
-ax1.tick_params(length = 0, label1On = False, label2On = False)
-#ax1.set_aspect(aspout)
-
-plt.plot(fil, color = opcolor)
-plt.ylabel("Filtered Output")
-plt.xlabel("Samples")
-plt.savefig("fil.png", transparent = True, dpi = DPI)
+plt.savefig("plot.png")
