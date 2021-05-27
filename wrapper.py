4cbccc1a23a0da58b85816b1ec9ebffd5d8ebf43
j-archit <archit.jain38@gmail.com>
2021-05-27 23:53:48 +0530

    Improved Plot, added CLI arguement customization ability

--- wrapper.py
+++ wrapper.py
@@ -1,34 +1,61 @@
-from os import system as sys
+from os import path, system as sys
 from scipy import signal
 import numpy as np
 import matplotlib.pyplot as plt
 import codecs
 from re import findall
+import getopt, sys as sys2
 
+from scipy.signal.signaltools import _compute_factors
 
-# Design Control Parameters for - Filter and Testbench
+# Default Design Control Parameters for - Filter and Testbench
 params = {
-    "filter_type"   : "low",
-    "fs"            : 48000,
-    "fc"            : 5000,
-    "order"         : 2,
-    "bitwidth"      : 32,
-    "fac"           : 24,
-    "gainl"         : 0,
-    "gainm"         : 0,
-    "htp"           : 10 
+    # Model Params
+    "ftype"     : "low",
+    "fs"        : 48000,
+    "fc"        : 2000,
+    "order"     : 1,
+    "bitwidth"  : 32,
+    "fac"       : 20,
+    "gainl"     : 0,
+    "gainm"     : 0,
+    "htp"       : 10, 
+    # Plot Params
+    "infile"    : "out.txt",
+    "color1"    : '#444444',
+    "color2"    : '#4444AA',
+    "color3"    : '#44AA44',
+    "color4"    : '#AA6666',
+    "DPI"       : 300,
+    "l_loc"     : "lower left",
+    "l_fancy"   : True,
+    "l_box"     : (-0.02, -0.25),
+    "l_alpha"   : 0,
+    "transp"    : False,
+    "t_box"     : (1, 0),
+    "t_hor"     : "right",
+    "t_ver"     : "center_baseline"
 }
-plot_params = {
-    "infile"   : "out.txt",
-    "color1"    : '#000000',
-    "color2"    : '#000000',
-    "DPI"       : 300
-}
+DEV = False
+cfac = 1
 
 with open("inp.txt", "r") as p:
     ips = p.readlines()
 with open("filter_tb.v", "r") as f:
     tb_lines = f.readlines()
+
+def norml(k):
+    maxx = max(k)
+    minn = min(k)
+    L = [(2*(val-minn)/(maxx-minn))-1 for val in k]
+    return L
+
+def mse(A,B):
+    E = [a-b for a,b in zip(A,B)]
+    mse = 0
+    for e in E:
+        mse = mse + e*e
+    return mse / len(E)
 
 def nbit_bin (a, n = 2*params["bitwidth"]):
     """Generate a n-bit binary representation of the input a
@@ -49,7 +76,36 @@ def nbit_bin (a, n = 2*params["bitwidth"]):
            binary = f"{(a<0)*1}"*(n - len(binary)) + binary 
     return binary
 
-def get_write_filter_coeffs(order, fs, fc, filter_type, fac, bitwidth, **extra):
+def getargs(args):
+    # Get Command Line Arguements
+    try:
+        opts = getopt.getopt(args, "t:o:c:s:b:f:l:m:d", ["type=", "order=", "cutoff=", "samplerate=", "bitwidth=", "fac=", "gainl=", "gainm="])
+        opts = opts[0]
+    except getopt.GetoptError:
+        print("Incorrect Usage of Flags, Reverting to Default Parameters")
+
+    for opt, arg in opts:
+        if opt == "-d":
+            global DEV
+            DEV = True
+        if opt in ("-t", "--type"):
+            params["ftype"] = arg
+        elif opt in ("-o", "--order"):
+            params["order"] = int(arg)
+        elif opt in ("-c", "--cutoff"):
+            params["fc"] = int(arg)
+        elif opt in ("-s", "--samplerate"):
+            params["fs"] = int(arg)
+        elif opt in ("-b", "--bitwidth"):
+            params["bitwidth"] = int(arg)
+        elif opt in ("-f", "--fac"):
+            params["fac"] = int(arg)
+        elif opt in ("-l", "--gainl"):
+            params["gainl"] = int(arg)
+        elif opt in ("-m", "--gainm"):
+            params["gainm"] = int(arg)
+
+def get_write_filter_coeffs(order, fs, fc, ftype, fac, bitwidth, **extra):
     """ Makes a filter depending on the inputs and 
     Writes the coefficients in the specified format 
     to coefficient files
@@ -58,7 +114,7 @@ def get_write_filter_coeffs(order, fs, fc, filter_type, fac, bitwidth, **extra):
         order (int)         : Order of the Filter
         fs (float)          : Sampling Frequency of the input samples
         fc (float)          : Cutoff Frequency of the required Filter
-        filter_type (str)   : "low", "high"
+        ftype (str)   : "low", "high"
         fac (int)           : Coefficient Left shift (2^fac) for the Hardware Implementation
         bitwidth (int)      : Bitwidth for the hardware Implementation
     
@@ -69,28 +125,22 @@ def get_write_filter_coeffs(order, fs, fc, filter_type, fac, bitwidth, **extra):
     
     fc_n = 2*fc/fs; # Normalised Frequency for Digital Filter
     
-    # This is necessary because the SciPy Library is doing some stupid stuff.
-    k = signal.butter(N = order, Wn = fc_n, btype = filter_type, output='zpk', analog = False)[2]
-    # print(f"Gain Factor: {k}")
- 
-    sos = signal.butter(N = order, Wn = fc_n, btype = filter_type, output='sos', analog = False)
+    sos = list(signal.butter(N = order, Wn = fc_n, btype = ftype, output='sos', analog = False))
+       
     # Compute Coefficient as required from raw values 
     # and write to Files as per required format
     for j, co in enumerate(sos):
         f = open(f"{str(int(j/10))}{str(j%10)}", mode = "w")
         line = ""
-        dline = ""
+        # dline = ""
         for i, val in enumerate(co):
-            if i < 3:
-                if j == 0 :
-                    val =  val / k # Because the first section was doing weird things
             if i == 3: # Skip the Fourth Coefficient Entirely
                 continue
-            dline = dline + str((val)) + " "
+            # dline = dline + str((val)) + " "
             line = line + nbit_bin(int(round(( val * (2**fac) ))), 2*bitwidth) + " "
         f.write(line)
     
-    return k, sos
+    return sos
 
 def write_testbench(order, fac, bitwidth, gainl, gainm, htp, **extra):
     vparams = {
@@ -127,10 +177,15 @@ def write_testbench(order, fac, bitwidth, gainl, gainm, htp, **extra):
     with open("filter_tb.v", "w") as f:
         f.writelines(new)
 
-def plot_everything(infile, color1, color2, DPI, sos, **extra):
+def plot_everything(
+    infile, color1, color2, color3, color4,
+    DPI, sos, l_loc, l_fancy, l_box, l_alpha,
+    ftype, fs, fc, order, bitwidth, fac,
+    gainl, gainm, transp, t_box, t_hor,
+    t_ver, **extra):
+
     ip = []
     op = []
-
     with codecs.open(infile, "r", "utf-8") as f:
         inf = f.readlines()
         for row in inf:
@@ -139,61 +194,72 @@ def plot_everything(infile, color1, color2, DPI, sos, **extra):
                 continue
             ip.append(int(data[0]))
             op.append(int(data[1]))
-            
-    # Unfiltered Input Sequence
-    plt.figure()
-    ax = plt.axes()
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
-    ax.spines['bottom'].set_color(color2)
-    ax.spines['left'].set_color(color2)
-    ax.yaxis.label.set_color(color2)
-    ax.xaxis.label.set_color(color2)
-    ax.tick_params(length = 0, label1On = False, label2On = False)
-
-    plt.plot(ip, color = color1)
-    plt.ylabel("Input")
-    plt.xlabel("Samples")
-    plt.savefig("input.png", transparent = True, dpi = DPI)
-
-    # Hardware Filtered Output Sequence
-    plt.figure()
-    ax1 = plt.axes()
-    ax1.spines['top'].set_visible(False)
-    ax1.spines['right'].set_visible(False)
-    ax1.spines['bottom'].set_color(color2)
-    ax1.spines['left'].set_color(color2)
-    ax1.yaxis.label.set_color(color2)
-    ax1.xaxis.label.set_color(color2)
-    ax1.tick_params(length = 0, label1On = False, label2On = False)
-
-    plt.plot(op, color = color1)
-    plt.ylabel("Filtered Output")
-    plt.xlabel("Samples")
-    plt.savefig("output.png", transparent = True, dpi = DPI)
-
     # Software Ref Output Sequence
     ip = np.array(ip)
-    filtered = signal.sosfilt(sos, ip)
-    plt.figure()
-    ax = plt.axes()
-    ax.spines['top'].set_visible(False)
-    ax.spines['right'].set_visible(False)
-    ax.spines['bottom'].set_color(color2)
-    ax.spines['left'].set_color(color2)
-    ax.yaxis.label.set_color(color2)
-    ax.xaxis.label.set_color(color2)
-    ax.tick_params(length = 0, label1On = False, label2On = False)
+    soft_filt = signal.sosfilt(sos, ip)
 
-    plt.plot(filtered, color = color2)
-    plt.ylabel("Reference Output")
-    plt.xlabel("Samples")
-    plt.savefig("output_ref.png", transparent = True, dpi = DPI)
+    # Normalise all  in (-1, 1)
+    ip = norml(ip)
+    op = norml(op)
+    soft_filt = norml(soft_filt)
+    MSE = mse(op, soft_filt)
+
+    # Get Plots
+    figure, axs = plt.subplots(2,1, sharex=True, sharey=False)
+    # Plot Input Sequence
+    axs[0].tick_params(length = 0, label1On = False, label2On = False)
+    axs[0].spines["right"].set_visible(False)
+    axs[0].spines["top"].set_visible(False)
+    axs[0].spines["left"].set_visible(False)
+    axs[0].spines["bottom"].set_visible(False)
+    axs[0].set_title("Input")
+    axs[0].plot(ip, color = color1)
+    
+    # Plot Filtered Output Sequence - Hardware and Software Reference
+    axs[1].tick_params(length = 0, label1On = False, label2On = False)
+    axs[1].spines["right"].set_visible(False)
+    axs[1].spines["top"].set_visible(False)
+    axs[1].spines["left"].set_visible(False)
+    axs[1].spines["bottom"].set_visible(False)
+    axs[1].set_title("Output")
+    axs[1].plot([(m-r)/cfac for m,r in zip(op,soft_filt)], 
+                color = color4, label = "Err", alpha = 0.1)
+    axs[1].plot(soft_filt, color = color2, label = "Ref")
+    axs[1].plot(op, color = color3, label = "Model")
+    axs[1].set_ylim(top = 1.2, bottom = -1.2)
+    axs[1].legend(loc = l_loc, fancybox = l_fancy, 
+                bbox_to_anchor = l_box, framealpha = l_alpha)
+    
+    # Construct Plot Text
+    ptxt = f"Order {order}\n{ftype.capitalize()}pass\n{fc/1000}/{fs/1000} kHz\nMSE = {MSE:e}"
+    
+    axs[1].text(*t_box, ptxt, ha = t_hor, va = t_ver, 
+                transform=axs[1].transAxes, bbox = dict(facecolor = '#FFFFFF', alpha = l_alpha))
+    
+    figure.subplots_adjust(bottom = 0.15, left = 0.08)
+    savefile = path.join("results",
+        f"_{ftype}_{order}_({fc}_{fs})_{bitwidth}_{fac}_{gainl}_{gainm}.png")
+    plt.savefig(savefile, transparent = transp, dpi = DPI)
+    plt.close()
+
+def run():
+    sos = get_write_filter_coeffs(**params)
+    write_testbench(**params)
+    sys("iverilog -o ivop filter_tb.v iir_N.v iir_2.v")
+    sys("vvp ivop > out.txt")
+    plot_everything(sos = sos, **params)
+    sys("cls")
 
 # Run Everything
 if __name__ == "__main__":
-    k, sos = get_write_filter_coeffs(**params)
-    write_testbench(**params)
-    sys(r".\run.bat")
-    plot_everything(sos = sos, **plot_params)
-    sys("cls")
+    getargs(sys2.argv[1:])
+    if not DEV: run()
+    else:
+        for fc in range(8, 11):
+            params["fc"] = fc*1000
+            for order in range(1,6):
+                params["order"] = order
+                try: 
+                    run()
+                except:
+                    continue
